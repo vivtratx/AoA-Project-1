413af1c0ef3efcb6f5853bdb57743401dca5b3ac
viviantranatx <viviantranatx@gmail.com>
2025-10-03 13:24:47 -0500

    Added points array

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import csv
 
+
+# Load all the points into an array of points
+points = []
+
 with open('input.csv', mode='r') as file:
     csvFile = csv.reader(file)
-    for lines in csvFile:
-        print(lines)
+    for line in csvFile:
+        temp = tuple(line)
+        points.append(temp)
+
+
+# Main Logic goes down here:
+print(points)
+
+
